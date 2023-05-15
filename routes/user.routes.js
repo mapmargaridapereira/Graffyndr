9f843ac6a887cf23eba0b22a7a3f7fb25b918eb5
Margarida Pereira <map.margaridapereira@gmail.com>
2023-05-15 12:53:56 +0100

    added navbar

---
+++ routes/user.routes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require("../models/User.model");
+
+const fileUploader = require("../config/cloudinary.config");
